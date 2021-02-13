3c004f4970afc9615f903a1fff37a37e937880d0
Thiago Negreiros <thiago_vieira@cal-comp.com.br>
2021-02-13 09:54:59 -0300

    notes-app leasson 04 | Printing in color using Chalk

---
+++ notes-app/chalk.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk');
+const validator = require('validator');
+
+const validateEmail = (email) => {
+  return (validator.isEmail(email))? 
+          console.log(chalk.black.bold.bgGreen('Success!')) : 
+          console.log(chalk.bgRed('Wrong!'));
+}
+
+
+validateEmail('thiago@thiago.com.br');
+validateEmail('thiago.com.br');
