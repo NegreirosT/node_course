50fe9b38753d695c1523dd6eb28bad0d2f42dee7
Thiago Negreiros <negreiros.t@gmail.com>
2021-02-15 16:59:14 -0300

    Session 4 | Leasson 1 - Getting input from Users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,14 @@
-const getNotes = require('./notes.js')
+// const getNotes = require('./notes.js')
 
-const result = getNotes('just test notes');
+// const result = getNotes('just test notes');
 
-console.log(result);
+// console.log(result);
+
+
+/* Getting input from Users */
+const command =  process.argv[2];
+
+if(command === 'add'){
+    console.log('Adding new note!');
+}
+
