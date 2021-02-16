15c56cfe861709fa34eae31d430fe74eac7e7add
Thiago Negreiros <thiago_vieira@cal-comp.com.br>
2021-02-16 17:04:58 -0300

    notes-app session 04 | Add Note

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,62 @@
-// const getNotes = require('./notes.js')
+const notes = require('./notes.js');
+const yargs = require('yargs');
 
-// const result = getNotes('just test notes');
+//* Customize yargs Version
+yargs.version('1.1.0')
 
-// console.log(result);
+//* Create Add Command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true, //? Faz o argumento ser obrigatorio
+      type: 'string'
+    },
+    body: {
+      describe: 'Note description',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv){
+    notes.addNotes(argv.title, argv.body)
+  }
+})
 
+//* Create remove Command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  builder:{
+    title:{
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string'
+    }
+  },    
+  handler: function(argv){
+    notes.removeNotes(argv.title);
+  }
+})
 
-/* Getting input from Users */
-const command =  process.argv[2];
+//* Create read Command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function(){
+    console.log('Reading the note')
+  }
+})
 
-if(command === 'add'){
-    console.log('Adding new note!');
-}
+//* Create list Command
+yargs.command({
+  command: 'list',
+  describe: 'List a note',
+  handler: function(){
+    console.log('Listing the notes')
+  }
+})
 
+yargs.parse();
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,56 @@
+const fs = require('fs');
+const { array } = require('yargs');
+
 const getNotes = (notes) => {
   return 'Your notes is: '+ notes;
 }
 
-module.exports = getNotes;
+const addNotes = (title, body) => {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter((note) => {
+    return note.title === title
+  })
+
+  if(duplicateNotes.length === 0){
+    notes.push({
+      title: title,
+      body: body
+    })
+    
+    saveNotes(notes)
+    console.log('New note added!');
+  }else{
+    console.log('Note title taken!');
+  }
+
+}
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = () => {
+  try{
+    const readFile = fs.readFileSync('notes.json');
+    const dataJSON = readFile.toString();
+    return JSON.parse(dataJSON);
+  }catch(e){
+    return [];
+  }
+}
+
+const removeNotes = (title) => {
+  const notes = loadNotes()
+  const notesToKeep = notes.filter((note) => {
+    return note.title !== title
+  }) 
+
+  saveNotes(notesToKeep);
+}
+
+module.exports = {
+  getNotes: getNotes,
+  addNotes: addNotes,
+  removeNotes: removeNotes
+};
