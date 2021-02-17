d1a432b527069c701ab52379ee42753e89967f87
Thiago Negreiros <negreiros.t@gmail.com>
2021-02-16 21:08:02 -0300

    Notes App - Session 4 | Refactoring using Arrow function

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -7,9 +7,7 @@ const getNotes = (notes) => {
 
 const addNotes = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter((note) => {
-    return note.title === title
-  })
+  const duplicateNotes = notes.filter((note) => note.title === title)
 
   if(duplicateNotes.length === 0){
     notes.push({
@@ -42,9 +40,7 @@ const loadNotes = () => {
 
 const removeNotes = (title) => {
   const notes = loadNotes()
-  const notesToKeep = notes.filter((note) => {
-    return note.title !== title
-  }) 
+  const notesToKeep = notes.filter((note) => note.title !== title) 
 
   if(notes.length > notesToKeep.length){
     console.log(chalk.green.inverse('Note removed!'));
